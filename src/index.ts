24750e5df4afafd90c87dcd2a809f721776637e6
celli33 <limon.martinez.misael@gmail.com>
2021-06-23 17:23:21 -0500

    build: added v1.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-/** Hello world */
+import { ConsumerClientInterface } from './Contracts/ConsumerClientInterface';
+import { ConsumerClientResponseInterface } from './Contracts/ConsumerClientResponseInterface';
+import { Consumer } from './Consumer';
+import { ConsumerClientResponse } from './Utils/ConsumerClientResponse'
+
+export { ConsumerClientInterface, ConsumerClientResponseInterface, Consumer, ConsumerClientResponse };
+
+Object.assign(module.exports, Consumer);
+Object.assign(module.exports, ConsumerClientResponse);
